7d7d32b1b4bcc824974006338cba677eaebc810b
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-24 16:48:04 +0100

    create array

--- server.js
+++ server.js
@@ -2,6 +2,39 @@ const express = require ('express')
 const app = express()
 const PORT = 3000 
 
+const array = [ 
+    {
+        titolo:,
+        contenuto: ,
+        immagine: ,
+        tags : []
+    },
+     {
+        titolo:,
+        contenuto: ,
+        immagine: ,
+        tags : []
+    },
+     {
+        titolo:,
+        contenuto: ,
+        immagine: ,
+        tags : []
+    },
+     {
+        titolo:,
+        contenuto: ,
+        immagine: ,
+        tags : []
+    },
+     {
+        titolo:,
+        contenuto: ,
+        immagine: ,
+        tags : []
+    }
+]
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port http://localhost:${PORT}`);  
 })
